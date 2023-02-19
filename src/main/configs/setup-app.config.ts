9f1810b39df0491a26845a9b231b05e6fb7cb558
Erico Almeida <erico.silva@invillia.com>
2023-02-19 19:08:37 -0400

    feat: add setup routes

--- src/main/configs/setup-app.config.ts
+++ src/main/configs/setup-app.config.ts
@@ -1,11 +1,13 @@
 import express, { type Express } from 'express'
 
 import { setupMiddlewares } from '@main/configs/setup-middlewares.config'
+import { setupRoutes } from '@main/configs/setup-routes.config'
 
 const setupApp = (): Express => {
   const app = express()
 
   setupMiddlewares(app)
+  setupRoutes(app)
 
   return app
 }
---
+++ src/main/configs/setup-routes.config.ts
@@ -0,0 +1,5 @@
+import { type Express } from 'express'
+
+const setupRoutes = (app: Express): void => {}
+
+export { setupRoutes }
